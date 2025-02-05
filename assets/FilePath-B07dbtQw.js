829d357775f38cd28671929300ac120646acf6ff
supernum163 <supernum@163.com>
2025-02-05 19:34:28 +0800

    模板语法树简介

---
+++ assets/FilePath-B07dbtQw.js
@@ -0,0 +1,1 @@
+import{a,o,c as n,i as s}from"./index-BvYZVGEi.js";import{_ as p}from"./Index-D1RlcXCn.js";const r=["innerHTML"],c=a({__name:"FilePath",props:["path"],setup(t){const e=t;return(_,i)=>e.path?(o(),n("span",{key:0,class:"file-path",innerHTML:e.path},null,8,r)):s("",!0)}}),h=p(c,[["__scopeId","data-v-03a99214"]]);export{h as F};
